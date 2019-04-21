d6f5b991820d1ae8d14f3a4e838c235baf77badd
Shiva <mahdavianshiva@gmail.com>
2019-04-21 21:27:19 +0430

    Indent

--- tb_transmitter.vhd
+++ tb_transmitter.vhd
@@ -19,13 +19,13 @@ END tb_transmitter;
 ARCHITECTURE test OF tb_transmitter IS
     -------- DUT declaration: --------
     COMPONENT transmitter IS
-    PORT(
-        start_in : IN  std_logic;
-        data_in  : IN  std_logic_vector(7 DOWNTO 0);
-        clk      : IN  std_logic;
-        nreset   : IN  std_logic;
-        s_out    : OUT std_logic
-        );
+        PORT(
+            start_in : IN  std_logic;
+            data_in  : IN  std_logic_vector(7 DOWNTO 0);
+            clk      : IN  std_logic;
+            nreset   : IN  std_logic;
+            s_out    : OUT std_logic
+            );
     END COMPONENT;
     -------- Signal declaration: --------
         SIGNAL clk_tb, nrst : std_logic;
@@ -51,22 +51,22 @@ BEGIN
     -------- Stimuli Generation: --------
     stimuli: PROCESS
     BEGIN
-        nrst <= '1';    WAIT FOR 10 ns;
+        nrst  <= '1';    WAIT FOR 10 ns;
         start <= '0';
-        nrst <= '0';    WAIT FOR 10 ns;
-        nrst <= '1';
-        data <= "11001100";
+        nrst  <= '0';    WAIT FOR 10 ns;
+        nrst  <= '1';
+        data  <= "11001100";
         start <= '1';   WAIT FOR 10 ns;
         start <= '0';   WAIT FOR 100 ns;
-        data <= "10001010";
+        data  <= "10001010";
         start <= '1';   WAIT FOR 10 ns;
         start <= '0';   WAIT FOR 100 ns;
-        data <= "10101010";
+        data  <= "10101010";
         start <= '1';   WAIT FOR 10 ns;
         start <= '0';   WAIT FOR 100 ns;
         start <= '1';   WAIT FOR 10 ns;
         start <= '0';   WAIT FOR 30 ns;
-        nrst <= '0';    WAIT FOR 20 ns;
+        nrst  <= '0';    WAIT FOR 20 ns;
         WAIT;
     END PROCESS stimuli;
 END test;
