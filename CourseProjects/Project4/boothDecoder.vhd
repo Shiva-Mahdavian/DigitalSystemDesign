ca2153b02567a9ad7d2fb9ba723ec1986a133d03
Shiva <mahdavianshiva@gmail.com>
2019-05-08 21:48:14 +0430

    Correct errors and change generic to 65

--- CourseProjects/Project4/boothDecoder.vhd
+++ CourseProjects/Project4/boothDecoder.vhd
@@ -9,12 +9,12 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 -----------------------------------------------------------------
 ENTITY boothDecoder IS
-	GENERIC (N : integer := 64);
+	GENERIC (N : integer := 65);
 	PORT(
 		a_in  			 : IN  std_logic_vector(2 DOWNTO 0);
-		y_in, y_l_in	 : IN  std_logic_vector(N DOWNTO 0);   -- Multiplicand and -Multiplicand
-		y2_in, y2_l_in   : IN  std_logic_vector(N DOWNTO 0);   -- 2*Multiplicand and -2*Multiplicand
-		z_out 			 : OUT std_logic_vector(N DOWNTO 0)
+		y_in, y_l_in	 : IN  std_logic_vector(N-1 DOWNTO 0);   -- Multiplicand and -Multiplicand
+		y2_in, y2_l_in   : IN  std_logic_vector(N-1 DOWNTO 0);   -- 2*Multiplicand and -2*Multiplicand
+		z_out 			 : OUT std_logic_vector(N-1 DOWNTO 0)
 		);
 END boothDecoder;
 -----------------------------------------------------------------
@@ -24,23 +24,19 @@ BEGIN
 	BEGIN
 		CASE a_in IS
 			WHEN "000" =>
-				z_out <= "0";
-			WHEN "001" =>
-				z_out <= y_in;
-			WHEN "010" =>
+				z_out <= (OTHERS => '0');
+			WHEN "001"|"010" =>
 				z_out <= y_in;
 			WHEN "011" =>
 				z_out <= y2_in;
 			WHEN "100" =>
 				z_out <= y2_l_in;
-			WHEN "101" =>
-				z_out <=  y_l_in;
-			WHEN "110" =>
+			WHEN "101"|"110" =>
 				z_out <=  y_l_in;
 			WHEN "111" =>
-				z_out <= "0";
+				z_out <= (OTHERS => '0');
 			WHEN OTHERS =>
-				z_out <= "1";
+				z_out <= (OTHERS => '0');
 		END CASE;
 	END PROCESS decode;
 END decoder;
